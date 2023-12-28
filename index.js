0def6574f13f674626b8f22ca1c535a7e3e5e683
axel <axel_nomore@hotmail.com>
2023-12-28 19:24:23 -0400

    refactor() add teste

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
+// onst PORT = process.env.PORT || 3000;
+
+
+var ambiente   = process.env.AMBIENTE;
+
+app.use(express.json())
+
+
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+
+app.get(`/teste`, async (req, res) => {
+
+    res.json({ 
+        "name": "teste", 
+        "env": ambiente  
+    });
+});
+
+
+
 app.listen(process.env.PORT || 3000)
